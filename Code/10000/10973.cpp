4e5af3092925eaf1a184799f1ffd1065dbcbcb26
MinetaS <31549467+MinetaS@users.noreply.github.com>
2019-06-09 14:36:11 +0900

    Solved 10973

---
+++ Code/10000/10973.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n;
+	vector<int> p;
+
+	scanf("%d", &n);
+	p = vector<int>(n);
+
+	for (int i=0 ; i<n ; i++) scanf("%d", &p[i]);
+
+	if (prev_permutation(p.begin(), p.end()))
+		for (int &e : p) printf("%d ", e);
+	else printf("-1");
+
+	return 0;
+}
