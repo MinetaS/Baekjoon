194c4dae57520f5cfc91f97ccff928f71c84c275
MinetaS <31549467+MinetaS@users.noreply.github.com>
2019-06-26 11:27:04 +0900

    Solved 17294

---
+++ Code/17000/17294.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string s;
+	int d;
+
+	cin >> s;
+	d = s[1]-s[0];
+
+	for (int i=2 ; i<s.size() ; i++) {
+		if (s[i]-s[i-1] != d) {
+			cout << "흥칫뿡!! <(￣ ﹌ ￣)>";
+			return 0;
+		}
+	}
+
+	cout << "◝(⑅•ᴗ•⑅)◜..°♡ 뀌요미!!";
+
+	return 0;
+}
