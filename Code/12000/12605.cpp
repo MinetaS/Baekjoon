e542c8913154e5f641efaa11e58f5f9ccdac437e
MinetaS <skdty87@gmail.com>
2019-12-09 03:04:25 +0900

    Solved 12605

---
+++ Code/12000/12605.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <sstream>
+#include <deque>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int _case;
+
+	cin >> _case;
+	cin.ignore(2147483647, 10);
+
+	for (int _=0 ; _<_case ; _++) {
+		string line;
+
+		getline(cin, line);
+		
+		deque<string> list;
+		stringstream st(line);
+		string s;
+
+		cout << "Case #" << _+1 << ": ";
+
+		while (st >> s) list.push_back(s);
+		while (!list.empty()) {
+			cout << list.back() << " ";
+			list.pop_back();
+		}
+
+		cout << '\n';
+	}
+
+	return 0;
+}
